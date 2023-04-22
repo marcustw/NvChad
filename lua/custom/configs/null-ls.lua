103d276b683703ad7729290cd7553a3cdfc6a6c3
marcustw <marcustanwei1@gmail.com>
2023-04-23 00:44:56 +0800

    fix: add autoformatting on save for my tech stacks

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -10,16 +10,41 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with {
+    filetypes = { "html", "markdown", "css", "typescript", "tsx", "javascript", "json", "yaml" },
+  }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
 
   -- cpp
   b.formatting.clang_format,
+
+  -- python
+  b.formatting.black,
+  b.formatting.isort,
 }
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format {
+            bufnr = bufnr,
+            filter = function(cl)
+              return cl.name == "null-ls"
+            end,
+          }
+        end,
+      })
+    end
+  end,
 }
